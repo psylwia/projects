b3641801fc4039b2adf5c294f19de5f821e98f75
Sylwia Pawlak <psylwia@gmail.com>
2017-03-08 17:19:09 +0100

    Style

--- Travel_medicine/js/app.js
+++ Travel_medicine/js/app.js
@@ -1,51 +1,31 @@
+
 $(document).ready(function(){
+// Slider
   $('.bxslider').bxSlider();
 
-    
-$(".map_image a").click(function() {
-    var countryDesc = $(this).data('description');
-    var test = $('.test');   
+// Zawartość okna z informacją o szczepieniach 
+    $(".map_image a").click(function() {
+        var countryDesc = $(this).data('description');
+        var test = $('.test');   
     
     var newDiv = $("<div>" + countryDesc + "</div>");
- 
     test.after(newDiv);
-    
+ 
+// Funkcjonalność guzika zamknięcia okna
     $('.button_x').click(function(){
         $('.vax_info').addClass('hidden');
         newDiv.empty();
     });
        
     });       
-    
+   
+// Podświetlenie aktywnej strony    
     var url = window.location.href; 
 
-    // passes on every "a" tag 
     $("a").each(function() {
-            // checks if its the same on the address bar
         if(url == (this.href)) { 
             $(this).closest("li").addClass("active");
         }
     });
 });
 
-var map = document.querySelector('#map');
-var paths = map.querySelectorAll('.map_image a');
-var vax = map.querySelector('.map_image section');
-var button = document.querySelector('.button_x');
-var test = document.querySelector('.test');
-
-paths.forEach(function (path) {
-   path.addEventListener('mouseenter', function(e){
-      var id = this.id.replace('country_', '');
-       map.querySelectorAll('.active').forEach(function(item){
-           item.classList.remove('active');
-       });
-       
-      document.querySelector('#country_' + id).classList.add('active');
-   });
-   
-   path.addEventListener('click', function(e){
-       vax.classList.remove('hidden');
-   }); 
-
-});
--- Travel_medicine/porady.php
+++ Travel_medicine/porady.php
@@ -20,15 +20,16 @@
 <body>
     <header>
         <div class="container clearfix">
-              <div id="logo_menu"><a href="index.html">Poradnia Medycyny Podróży</a></div>
+            <div id="logo_menu"><a href="index.html">Poradnia Medycyny Podróży</a>
+            </div>
             <nav>
-            <ul>
-                <li><a href="index.html">Strona główna</a></li>
-                <li><a href="cennik.html">Cennik</a></li>
-                <li><a href="porady.php">Porady dla podróżujących</a></li>
-                <li><a href="certyfikat.html">Certyfikat</a></li>
-                <li><a href="kontakt.html">Kontakt</a></li>
-            </ul>
+                <ul>
+                    <li><a href="index.html">Strona główna</a></li>
+                    <li><a href="cennik.html">Cennik</a></li>
+                    <li><a href="porady.php">Porady dla podróżujących</a></li>
+                    <li><a href="certyfikat.html">Certyfikat</a></li>
+                    <li><a href="kontakt.html">Kontakt</a></li>
+                </ul>
             </nav>
         </div>
         
@@ -36,45 +37,39 @@
     
     <section class="section"> 
         <div class="container clearfix">
-                <ul class="bxslider">
-              <li><img src="images/1.jpg" /></li>
-              <li><img src="images/2.jpg" /></li>
-              <li><img src="images/3.jpg" /></li>
-              <li><img src="images/4.jpg" /></li>
-              <li><img src="images/5.jpg" /></li>
-              <li><img src="images/6.jpg" /></li>
+            <ul class="bxslider">
+                <li><img src="images/1.jpg" /></li>
+                <li><img src="images/2.jpg" /></li>
+                <li><img src="images/3.jpg" /></li>
+                <li><img src="images/4.jpg" /></li>
+                <li><img src="images/5.jpg" /></li>
+                <li><img src="images/6.jpg" /></li>
             </ul>
 
                 
-                <div class='map' id="map">
-                    <h1>Mapa szczepień</h1>
+            <div class='map' id="map">
+                <h1>Mapa szczepień</h1>
                     
-                    <div class="map_image">
-
-                   <svg xmlns="http://www.w3.org/2000/svg" xmlns:amcharts="http://amcharts.com/ammap" xmlns:xlink="http://www.w3.org/1999/xlink" version="1.1" viewBox="0 0 1005 669">
+                <div class="map_image">
+                    <svg xmlns="http://www.w3.org/2000/svg" xmlns:amcharts="http://amcharts.com/ammap" xmlns:xlink="http://www.w3.org/1999/xlink" version="1.1" viewBox="0 0 1005 669">
                        
-                       <?php include('baza.php');?>
-</svg>
+                    <?php include('baza.php');?>
+                    </svg>
 
                         
-        <section class="vax_info hidden">
-                <svg class="button_x">
-                    <circle cx="12" cy="12" r="11" stroke="black" stroke-width="2" fill="white" />
-                    <path stroke="black" stroke-width="4" fill="none" d="M6.25,6.25,17.75,17.75" />
-                    <path stroke="black" stroke-width="4" fill="none" d="M6.25,17.75,17.75,6.25" />
-                </svg>   
-            <div class="test">
-                
+                    <section class="vax_info hidden">
+                        <svg class="button_x">
+                            <circle cx="12" cy="12" r="11" stroke="black" stroke-width="2" fill="white" />
+                            <path stroke="black" stroke-width="4" fill="none" d="M6.25,6.25,17.75,17.75" />
+                            <path stroke="black" stroke-width="4" fill="none" d="M6.25,17.75,17.75,6.25" />
+                        </svg>   
+                        <div class="test"></div>
+                    </section>
+                </div>   
             </div>
-        </section>
-                        
-                        
-                    </div>
-                    
-                </div>
             
        
-                </div>   
+        </div>   
     </section>
     
     <footer>
