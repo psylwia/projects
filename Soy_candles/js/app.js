f5e08441d8af5217c9e00641248ce54b30c45aa5
Sylwia Pawlak <psylwia@gmail.com>
2017-03-22 14:30:22 +0100

    Newsletter popup

--- Soy_candles/js/app.js
+++ Soy_candles/js/app.js
@@ -1,3 +1,18 @@
 $(document).ready(function(){
-  $('.bxslider').bxSlider();
+    $('.bxslider').bxSlider();
+
+
+    $(function(){
+       function show_popup(){
+          $(".popup").removeClass("hidden");
+       };
+       window.setTimeout( show_popup, 5000 ); // 5 seconds
+        
+    });
+    
+     $(".button_x").click(function(){
+        $('.popup').addClass('hidden');
+    }); 
 });
+
+  
