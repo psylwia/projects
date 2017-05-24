4a75889294395b85995c96c6a251e7e0418ee257
Sylwia Pawlak <psylwia@gmail.com>
2017-05-24 23:49:19 +0200

    Portfolio header

--- Portfolio/js/app.js
+++ Portfolio/js/app.js
@@ -1,35 +1,10 @@
 $(document).ready(function(){
-    $('.bxslider').bxSlider();
+    $(document).on('click', 'a', function(event){
+        event.preventDefault();
 
-
-    $(function(){
-       function show_popup(){
-          $(".popup").removeClass("hidden");
-       };
-       window.setTimeout( show_popup, 5000 ); // 5 seconds
-        
-    });
-    
-     $(".button_x").click(function(){
-        $('.popup').addClass('hidden');
-    }); 
-    
-    var stickyNavTop = $('.menu').offset().top;
- 
-    var stickyNav = function(){
-    var scrollTop = $(window).scrollTop();
-
-    if (scrollTop > stickyNavTop) { 
-        $('.nav').addClass('sticky').removeClass('hidden');
-    } else {
-        $('.nav').removeClass('sticky').addClass('hidden'); 
-    }
-    };
-
-    stickyNav();
-
-    $(window).scroll(function() {
-      stickyNav();
+        $('html, body').animate({
+            scrollTop: $( $.attr(this, 'href') ).offset().top
+        }, 500);
     });
 });
 
